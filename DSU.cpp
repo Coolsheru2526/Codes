04913f03d0304afa36eb59b31546fd3223b2e9a3
Coolsheru2526 <shreyanshmishra.4365@gmail.com>
2024-08-26 10:38:59 +0530

    some changes

--- DSU.cpp
+++ DSU.cpp
@@ -10,7 +10,7 @@ class Disjoint{
    {
     rank.resize(n+1,0);
     parent.resize(n+1); 
-     size.resize(n+1,1); 
+    size.resize(n+1,1); 
     for(int i=0;i<=n;i++)
     {
         parent[i]=i;
